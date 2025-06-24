7c2cb6d1640fb1926126f8b466881c1a9bc7f6e7
Antania Han <lemonart.sby@gmail.com>
2025-06-24 13:06:42 +0700

    tambah jajargenjang

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//=== Segitiga ===
 const formLuas = document.getElementById("form-luas");
 formLuas.addEventListener("submit",function (event) {
     event.preventDefault();
@@ -17,6 +18,7 @@ formLuas.addEventListener("submit",function (event) {
 
     const luas = 0.5 * alas * tinggi;
     hasilLuas.value = (Math.round(luas*100)/100).toString();
+});
 
     document.getElementById("resetLuas").addEventListener("click",function(){
         document.getElementById("alas").value="";
@@ -24,10 +26,7 @@ formLuas.addEventListener("submit",function (event) {
         document.getElementById("luas").value="";
     });
 
-});
-
 const formKeliling = document.getElementById("form-keliling");
-
 formKeliling.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -47,6 +46,7 @@ formKeliling.addEventListener("submit", function (event) {
 
     const keliling = sisiA + sisiB + sisiC;
     hasilKeliling.value = (Math.round(keliling*100)/100).toString();
+});
 
     document.getElementById("resetKeliling").addEventListener("click",function(){
         document.getElementById("sisi-a").value="";
@@ -55,4 +55,34 @@ formKeliling.addEventListener("submit", function (event) {
         document.getElementById("keliling").value="";
     });
 
+
+//=== Jajar genjang ===
+const formluasJG = document.getElementById("form-luas-jg");
+formluasJG.addEventListener("submit",function (event) {
+    event.preventDefault();
+
+    console.log("Form Jajar Genjang disubmit!");
+    
+    const alas = parseFloat(document.getElementById("jg-alas").value.replace(",","."));
+    const tinggi = parseFloat(document.getElementById("jg-tinggi").value.replace(",","."));
+    const hasilLuas = document.getElementById("luas-jg");
+
+    if(isNaN(alas) || isNaN(tinggi)) {
+        alert("Masukkan angka yang valid untuk alas dan tinggi!");
+        return;
+    }
+    if (alas <= 0 || tinggi <=0) {
+        alert("Angka tidak boleh nol atau negatif!");
+        return;
+    }
+
+    const luas = alas * tinggi;
+    hasilLuas.value = (Math.round(luas*100)/100).toString();
 });
+
+    document.getElementById("resetLuas-jg").addEventListener("click",function(){
+        document.getElementById("jg-alas").value="";
+        document.getElementById("jg-tinggi").value="";
+        document.getElementById("luas-jg").value="";
+    });
+
